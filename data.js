d5df5b63202731a8908aa1e71e4e8a547484c57a
akhyar00 <149314626+akhyar00@users.noreply.github.com>
2025-10-01 11:14:10 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,62 @@
+// ===================================================================
+// DATA PENGURUS HMPS PAI STAI PATI PERIODE 2025
+// ===================================================================
+const dataPengurus = {
+    "bph": [
+        { nama: "Febriyanto Indra Kurniawan", jabatan: "Ketua Umum", foto: "default.png" },
+        { nama: "Nadila Ayu Wardani", jabatan: "Sekretaris 1", foto: "default.png" },
+        { nama: "Fathimatuz Zahro'", jabatan: "Sekretaris 2", foto: "default.png" },
+        { nama: "Lutfiani Zahra Safilla", jabatan: "Bendahara 1", foto: "default.png" },
+        { nama: "Erika Maulani Putri", jabatan: "Bendahara 2", foto: "default.png" }
+    ],
+    "pendidikan": {
+        koordinator: { nama: "Muhammad Haizun Ni'am", jabatan: "Koordinator", foto: "default.png" },
+        anggota: [
+            { nama: "Anandita Itiqomah", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Siti Nur Fasanah", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Cindi Afriyanti", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Tiara Ramadhani", jabatan: "Anggota", foto: "default.png" }
+        ]
+    },
+    "infokom": {
+        koordinator: { nama: "Ilham Pratama", jabatan: "Koordinator", foto: "default.png" },
+        anggota: [
+            { nama: "Muhammad Amirul Mu'minin", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Nur Kholiq Rizqi", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Nela Rofa'atul Kasanah", jabatan: "Anggota", foto: "default.png" }
+        ]
+    },
+    "keagamaan": {
+        koordinator: { nama: "Laili Nailur Rohmah", jabatan: "Koordinator", foto: "default.png" },
+        anggota: [
+            { nama: "Luthfiyatin Nur Inayah", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Dwi Nur Laili Maftuhatus Shofa", jabatan: "Anggota", foto: "default.png" },
+            { nama: "M. Aufa Anwaril Akbar", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Syamsul Huda Romadhoni", jabatan: "Anggota", foto: "default.png" }
+        ]
+    },
+    "kewirausahaan": {
+        koordinator: { nama: "Naila Lailatul Maghfuroh", jabatan: "Koordinator", foto: "default.png" },
+        anggota: [
+            { nama: "Ananda Putri Vinata", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Farid Uzhma Al-Imami", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Dhieta Ratu Nirmala", jabatan: "Anggota", foto: "default.png" },
+            { nama: "Durrotun Najwa", jabatan: "Anggota", foto: "default.png" }
+        ]
+    }
+};
+// ===================================================================
+// DATA PROGRAM KERJA (UNTUK SLIDER)
+// ===================================================================
+const dataProker = [
+    { judul: "Seminar Pendidikan Nasional", deskripsi: "Meningkatkan wawasan mahasiswa tentang tren pendidikan terkini.", gambar: "slide1.jpg" },
+    { judul: "Kajian Rutin Keislaman", deskripsi: "Memperdalam pemahaman keagamaan bagi mahasiswa PAI.", gambar: "slide2.jpg" },
+    { judul: "Bakti Sosial Masyarakat", deskripsi: "Kegiatan pengabdian kepada masyarakat di desa sekitar kampus.", gambar: "slide3.jpg" }
+];
+// ===================================================================
+// DATA ARTIKEL/GALERI KEGIATAN
+// ===================================================================
+const dataArtikel = [
+    { judul: "Suksesnya Acara Seminar Pendidikan 2025", tanggal: "30 Sep 2025", gambar: "artikel1.jpg", link: "#" },
+    { judul: "HMPS PAI Berbagi di Bulan Ramadhan", tanggal: "25 Sep 2025", gambar: "artikel2.jpg", link: "#" }
+];
